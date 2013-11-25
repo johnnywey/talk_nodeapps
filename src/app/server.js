00407b3dc2f832602e05a5a1999431a9ade855c3
Johnny Wey <johnnywey@gmail.com>
2013-11-25 11:18:20 -0700

    Add coverage

--- src/app/server.js
+++ src/app/server.js
@@ -1,10 +1,23 @@
-var http = require('http');
+'use strict';
+/** 
+ * Main entry point to our HTTP server.
+ */
+var connect = require('connect');
+var port = process.env.PORT || 8000;
 
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+// Connect config and app start
+var connectApp = connect()
+    .use(connect.bodyParser())
+    .use(connect.query())
+    .use(connect.cookieParser())
+    .use(connect.static('public'));
+
+connectApp.listen(port, function() {
+    var divider = '================================================================';
+    var startUpData = '\n' + divider;
+    startUpData += '\n=                          Sweet Web App                        =';
+    startUpData += '\n' + divider;
+    startUpData += '\nServer listening on [' + port + ']\n';
+    startUpData += divider;
+    console.log(startUpData);
 });
-
-server.listen(8000);
-
-console.log('Server listening on port 8000');
