7e2c09778973203e791985f774fe1370617d7773
Johnny Wey <johnnywey@gmail.com>
2013-11-25 11:16:41 -0700

    Add coverage

--- src/GruntFile.js
+++ src/GruntFile.js
@@ -11,6 +11,23 @@ module.exports = function(grunt) {
     ];
 
     grunt.initConfig({
+        clean: {
+            coverage: {
+                src: ['coverage/']
+            }
+        },
+        copy: {
+            test: {
+                src: ['test/**'],
+                dest: 'coverage/'
+            }
+        },
+        blanket: {
+            tasks: {
+                src: ['app/'],
+                dest: 'coverage/app/'
+            }
+        },
         jshint: {
             human: {
                 options: {
@@ -37,7 +54,23 @@ module.exports = function(grunt) {
                     require: 'should'
                 },
                 src: ['test/**/*.js']
-            }
+            },
+            coverage: {
+                options: {
+                    reporter: 'html-cov',
+                    require: 'should',
+                    // use the quiet flag to suppress the mocha console output
+                    quiet: true,
+                    captureFile: 'coverage.html'
+                },
+                src: ['coverage/test/**/*.js']
+            },
+            travis: {
+                options: {
+                    reporter: 'travis-cov'
+                },
+                src: ['coverage/test/**/*.js']
+            }            
         },
         watch: {
             test: {
@@ -51,6 +84,14 @@ module.exports = function(grunt) {
 
     grunt.registerTask('build', [
         'jshint:human'
+    ]);
+
+    grunt.registerTask('cover', [
+        'clean',
+        'copy',
+        'blanket',
+        'mochaTest:coverage',
+        'mochaTest:travis'
     ]);
 
     grunt.registerTask('default', 'build');
