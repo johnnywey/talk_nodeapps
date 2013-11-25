c97c39d5faf9e09f3c2786cf5b607f2a1224c643
Johnny Wey <johnnywey@gmail.com>
2013-11-25 11:12:14 -0700

    Add Mongoose

---
+++ src/app/models/models.js
@@ -0,0 +1,47 @@
+'use strict';
+/**
+ * Models.
+ */
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
+
+var DEFAULT_AVATAR_URL = '/images/no-avatar.png';
+
+// User roles we allow.
+var roles = (function() {
+    var _validRoles = {
+        internal: 'internal',
+        administrator: 'administrator',
+        user: 'user'
+    };
+
+    var _rolesArray = [];
+    for (var r in _validRoles) {
+        if (_validRoles.hasOwnProperty(r)) {
+            _rolesArray.push(r);
+        }
+    }
+
+    return {
+        validRoles: _validRoles,
+        rolesArray: _rolesArray
+    };
+})();
+
+/**
+ * User.
+ */
+var userSchema = mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    emailAddress: {type: String, required: true},
+    username: {type: String, index: {unique: true, dropDupes: true}, required: true},
+    password: {type: String, required: true},
+    roles: [
+        {type: String, 'enum': roles.rolesArray}
+    ],
+    imageUrl: {type: String, default: DEFAULT_AVATAR_URL, required: true},
+    active: {type: Boolean, default: true, required: true},
+    createTime: {type: Date, default: Date.now, required: true}
+});
+
+exports.User = mongoose.model('User', userSchema);
